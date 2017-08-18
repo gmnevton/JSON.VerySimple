8fce9e2bd4f6a2eee9d862814dd837e043fb0963
Grzegorz Molenda <gmnevton@gmail.com>
2017-08-18 12:46:04 +0200

    Add files via upload

---
+++ Tools/JSONParserTest/JSONParserTest.dpr
@@ -0,0 +1,15 @@
+program JSONParserTest;
+
+uses
+  FastMM4,
+  Forms,
+  uMain in 'uMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
